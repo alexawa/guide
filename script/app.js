878e975524e78c76daddb2a8f0306c21b4ec35df
Aleksandrs Bogackins <sashik3580@gmail.com>
2018-02-10 13:04:59 +0200

    stylized project version

--- script/app.js
+++ script/app.js
@@ -1,11 +1,25 @@
+;
+
+/*		Фиксированная-верхняя полоса прокрутки		*/
 $(window).scroll(function(){
 	var ratio = $(document).scrollTop() / (($(document).height() - $(window).height()) / 100);
 	$('#scrollBar-progress').width(ratio + '%');
 });
+/*		Фиксированная-верхняя полоса прокрутки		*/
 
+
+
+/*		Подключаем стилизацию для блоков кода		*/
+hljs.initHighlightingOnLoad();
+/*		Подключаем стилизацию для блоков кода		*/
+
+
+
+/*		Блоки кода в буфер обмена		*/
 function copyDivToClipboard() {
 	var range = window.getSelection().getRangeAt(0);
 	range.selectNode(document.getElementById("codeToClipboard"));
 	window.getSelection().addRange(range);
 	document.execCommand("copy");
 }
+/*		Блоки кода в буфер обмена		*/
